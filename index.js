e99cbfadb3be0049a42af34029b407c3d685bb74
Lucas-byte-python <lucassantil31@gmail.com>
2025-04-23 11:16:13 -0300

    update version 1.0

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ search.addEventListener('click', () => {
     if (city === '')
         return;
 
+    // Corrigido: uso de crase para interpolação
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
         .then(response => response.json())
         .then(json => {
@@ -38,26 +39,24 @@ search.addEventListener('click', () => {
                 case 'Clear':
                     image.src = 'images/clear.png';
                     break;
-
                 case 'Rain':
                     image.src = 'images/rain.png';
                     break;
-
                 case 'Snow':
                     image.src = 'images/snow.png';
                     break;
-
                 case 'Clouds':
                     image.src = 'images/cloud.png';
                     break;
-
                 case 'Haze':
+                case 'Mist':
                     image.src = 'images/mist.png';
                     break;
-
                 default:
                     image.src = '';
             }
+
+            // Corrigido: uso correto de template literals
             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
             description.innerHTML = `${json.weather[0].description}`;
             humidity.innerHTML = `${json.main.humidity}%`;
@@ -69,4 +68,10 @@ search.addEventListener('click', () => {
             weatherDetails.classList.add('fadeIn');
             container.style.height = '590px';
         });
+});
+
+document.querySelector('.search-box input').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        search.click();
+    }
 });
